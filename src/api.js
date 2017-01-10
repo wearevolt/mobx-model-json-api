8b58d710f7fea9f4de3e4bc8b8d592b13d8cfce3
staltec <staltec@gmail.com>
2017-01-10 13:54:45 +0700

    Revert: FIX: Nested array and object support in query string

--- src/api.js
+++ src/api.js
@@ -1,3 +1,6 @@
+import isString from 'mobx-model/node_modules/lodash/isString';
+import isArray from 'mobx-model/node_modules/lodash/isArray';
+import isPlainObject from 'mobx-model/node_modules/lodash/isPlainObject';
 import { API } from 'mobx-model';
 
 const request = API.request;
@@ -5,6 +8,42 @@ const jsonApiHeaders = {
   'Accept': 'application/vnd.api+json',
   'Content-Type': 'application/vnd.api+json',
 };
+
+
+function encodeParam (param) {
+  return isArray(param)
+    ? param.map(p => encodeURIComponent(p)).join()
+    : encodeURIComponent(param);
+}
+
+
+function toQueryStr (params) {
+
+  if (isString(params)) {
+    return `?${params}`;
+  }
+
+  if (!isPlainObject(params)) {
+    return '';
+  }
+
+  let paramsArr = Object.keys(params).map(key => {
+    const param = params[key];
+
+    if (!isPlainObject(param)) {
+      return `${key}=${encodeParam(param)}`;
+    }
+
+    return Object.keys(param).map(subKey => {
+      const subParam = param[subKey];
+
+      return `${key}[${subKey}]=${encodeParam(subParam)}`;
+    }).join('&');
+
+  });
+
+  return paramsArr.length ?  '?' + paramsArr.join('&') : '';
+}
 
 
 API.request = function (options = {}) {
@@ -21,13 +60,23 @@ API.request = function (options = {}) {
     superagent
   } = options;
 
+  const isGetMethod = /^get$/i.test(method);
+
   let endpointUrl = link || `${endpoint.replace(/_+/g, '-')}${id ? '/'+id : ''}`;
+  let requestParams = params;
+
+  if (isGetMethod) {
+    if (!link) {
+      endpointUrl += toQueryStr(params);
+    }
+    requestParams = undefined;
+  }
 
   return request.call(this, {
     method,
     endpoint: endpointUrl,
     requestHeaders: { ...this.requestHeaders, ...jsonApiHeaders },
-    data: params,
+    data: requestParams,
     superagent,
 
     onSuccess: (res = {}) => {
