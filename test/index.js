e1d40939fa2c733bb106c79b4f29c1a29ccd5f23
staltec <staltec@gmail.com>
2017-03-21 15:24:24 +0700

    Add tests for model relations

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 describe("Mobx-model JSON API", function() {
   require('./api');
   require('./base_model');
+  require('./relations');
   require('./restful_actions');
   require('./issues');
 });
